d9e8b371d60c5e1a581153560d1f7dadd8009921
MathewTheCoder <jmathew1201@live.com>
2021-03-20 19:02:47 +0530

    feat: basic layout created

---
+++ src/components/FormBuilder/FormBuilder.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FormBuilder = (): JSX.Element => {
+    return (
+        <main>
+            <section className="playGround">
+            </section>
+            <section className="toolBox">
+                <h1>Blocks</h1>
+            </section>
+        </main>
+    )
+}
+
+export default FormBuilder;
